511ede155b9b9c07f0fd8a3c82600e46ad6faf1d
Tashi Dolma <tadolma97@gmail.com>
2022-04-08 17:50:44 -0400

    commit tree eab8de3f56e179a08b482b79caff2818c0193506

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    cats.push('Ralph')}
+function destructivelyPrependCat(name){
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift()
+}
+
+function appendCat(){
+    const newCats=[...cats.slice(),'Broom']
+    return newCats
+}
+function prependCat(){
+    const newCats=['Arnold',...cats.slice()]
+    return newCats
+}
+function removeLastCat(){
+    const newCats=cats.slice(0,cats.length-1);
+    return newCats
+}
+function removeFirstCat(){
+    const newCats=cats.slice(1);
+    return newCats
+}
